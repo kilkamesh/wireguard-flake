9abcb90520d333aec97ff824f1af58beb4a95cba
VLAD KING (КОРОЛЬ) <nervcop@gmail.com>
2026-03-11 18:04:14 +0300

    fix

--- flake.nix
+++ flake.nix
@@ -4,18 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, sops-nix, ... }: {
-    nixosConfigurations = {
-      vps = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules =[
-          sops-nix.nixosModules.sops
-          ./wireguard.nix
-        ];
-      };
+    nixosModules.default = {
+      imports =[
+        sops-nix.nixosModules.sops
+        ./wireguard.nix
+      ];
     };
   };
 }
